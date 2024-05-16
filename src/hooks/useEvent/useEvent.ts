0024ba1aeb5e20fa5c3b786e518c9a987da8e198
khmilevoi <Khmilevoi@gmail.com>
2024-05-16 13:23:30 +0000

    implement useEvent hook

--- src/hooks/useEvent/useEvent.ts
+++ src/hooks/useEvent/useEvent.ts
@@ -1,11 +1,11 @@
 import { useCallback, useRef } from 'react';
 
-export function useEvent<T extends Function>(callback: T): T {
+export function useEvent<T extends (...args: any[]) => any>(callback: T): T {
   const callbackRef = useRef(callback);
 
   callbackRef.current = callback;
 
-  return useCallback(function (this: unknown, ...args: unknown[]) {
+  return useCallback(function (this: any, ...args: any[]) {
     return callbackRef.current.apply(this, args);
-  }, []) as unknown as T;
+  }, []) as T;
 }
