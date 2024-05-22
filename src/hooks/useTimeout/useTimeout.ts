9f8a568bcea1094a59481c0add10abe762561dad
triangle <ice.cool.full.beautiful@gmail.com>
2024-05-22 23:54:30 +0300

    Apply suggestions from code review
    Co-authored-by: Vitalij Ryndin <crashmax.dev@gmail.com>

--- src/hooks/useTimeout/useTimeout.ts
+++ src/hooks/useTimeout/useTimeout.ts
@@ -14,11 +14,15 @@ interface UseTimeoutReturn {
 }
 
 /**
- * Hook that executes a callback function after a specified delay.
+ * @name useTimeout
+ * @description Hook that executes a callback function after a specified delay.
  *
  * @param {() => void} callback - The function to be executed after the timeout.
  * @param {number} delay - The delay in milliseconds before the timeout executes the callback function.
  * @return {UseTimeoutReturn} An object with a `ready` boolean state value and a `clear` function to clear timeout.
+ *
+ * @example
+ * const { clear, ready } = useTimeout(() => {}, 5000)
  */
 export function useTimeout(callback: () => void, delay: number): UseTimeoutReturn {
   const [ready, setReady] = React.useState(false);
