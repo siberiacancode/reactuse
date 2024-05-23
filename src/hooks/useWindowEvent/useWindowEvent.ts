fd662e92bebe44bb8510c175b7f43ead6cf62d52
Andrey Fedoseev <a.fedoseev@sftpro.ru>
2024-05-23 10:26:40 +0300

    feat(docs): useWindowEvent

--- src/hooks/useWindowEvent/useWindowEvent.ts
+++ src/hooks/useWindowEvent/useWindowEvent.ts
@@ -1,6 +1,15 @@
 import type { UseEventListenerOptions } from '../useEventListener/useEventListener';
 import { useEventListener } from '../useEventListener/useEventListener';
 
+/**
+ * @name useWindowEvent
+ * @description - Attaches an event listener to the window object for the specified event.
+ *
+ * @param {Event} event - The event to listen for.
+ * @param {Function} listener - The callback function to be executed when the event is triggered.
+ * @param {UseEventListenerOptions} [options] - Optional configuration options for the event listener.
+ * @return {void}
+ */
 export const useWindowEvent = <Event extends keyof WindowEventMap>(
   event: Event,
   listener: (this: Window, event: WindowEventMap[Event]) => any,
