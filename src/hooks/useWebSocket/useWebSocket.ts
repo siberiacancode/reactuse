21617c3ddf2cd06b54b90d94828a22ca3aad4e5f
Алдияр Сейлханов <seylkhanov.aldiyar@gmail.com>
2024-05-26 13:47:09 +0500

    reworked hook, added jsdoc

--- src/hooks/useWebSocket/useWebSocket.ts
+++ src/hooks/useWebSocket/useWebSocket.ts
@@ -4,19 +4,45 @@ const connectSocket = (url: string) => {
   return new WebSocket(url);
 };
 
+interface IOptions {
+  onFail: () => void;
+  onSuccess: () => void;
+}
+
 /**
- * @name useWebSocket
- * @description Hook connects to a websocket server and returns a event
- * @param {url} string
- * @param {onMessage} callback
- * @returns {Ref}
+ * Custom React hook to connect to a WebSocket server.
+ *
+ * @function useWebSocket
+ * @param {string} url - The WebSocket server URL to connect to.
+ * @param {function} onMessage - Callback function to handle incoming WebSocket messages.
+ * @param {IOptions} [options] - Optional configuration object.
+ * @param {function} options.onFail - Callback function to execute when the WebSocket connection fails.
+ * @param {function} options.onSuccess - Callback function to execute when the WebSocket connection is successfully established.
+ * @returns {object} - An object containing the WebSocket client instance, a boolean indicating if the connection is open, and a function to send data through the WebSocket.
+ * @returns {WebSocket|null} return.client - The WebSocket client instance or null if not connected.
+ * @returns {boolean} return.open - A boolean indicating if the WebSocket connection is open.
+ * @returns {function} return.send - A function to send data through the WebSocket.
+ *
  * @example
- * useWebSocket('wss://serverws.com/', (message) => console.log(message));
+ * const { client, open, send } = useWebSocket('wss://serverws.com/', (message) => {
+ *   console.log(message.data);
+ * }, {
+ *   onFail: () => console.log('Connection failed'),
+ *   onSuccess: () => console.log('Connection successful')
+ * });
  */
-export const useWebSocket = (url: string, onMessage: (message: MessageEvent) => void) => {
+export const useWebSocket = (
+  url: string,
+  onMessage: (message: MessageEvent) => void,
+  options?: IOptions
+) => {
   const ws = useRef<WebSocket | null>(null);
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [waitingToReconnect, setWaitingToReconnect] = useState<any>(null);
+
+  const send = (data: string) => {
+    ws.current?.send(data);
+  };
 
   useEffect(() => {
     if (waitingToReconnect) return;
@@ -25,10 +51,18 @@ export const useWebSocket = (url: string, onMessage: (message: MessageEvent) => 
       const client = connectSocket(url);
       ws.current = client;
 
-      client.onerror = (e) => console.error('WebSocket error:', e);
+      client.onerror = (e) => {
+        if (options?.onFail) {
+          options?.onFail();
+        }
+        console.error('WebSocket error:', e);
+      };
 
       client.onopen = () => {
         setIsOpen(true);
+        if (options?.onSuccess) {
+          options?.onSuccess();
+        }
       };
 
       client.onmessage = (message) => {
@@ -58,5 +92,5 @@ export const useWebSocket = (url: string, onMessage: (message: MessageEvent) => 
     };
   }, [url, waitingToReconnect]);
 
-  return { client: ws.current, isOpen };
+  return { client: ws.current, open: isOpen, send };
 };
