b6d7c12cb7805a99cb7345f022ddf0ffc1807607
leendrew <leendrew99@gmail.com>
2024-05-26 11:19:50 +0600

    feat: hook demo

--- src/hooks/useStep/useStep.demo.tsx
+++ src/hooks/useStep/useStep.demo.tsx
@@ -2,44 +2,36 @@ import React from 'react';
 
 import { useStep } from './useStep';
 
-function Step1() {
+const Step1 = () => {
   return <p>This is Step 1</p>;
-}
+};
 
-function Step2() {
+const Step2 = () => {
   return <p>This is Step 2</p>;
-}
+};
 
-function Step3() {
+const Step3 = () => {
   return <p>This is Step 3</p>;
-}
+};
 
 const Demo = () => {
   const steps = [<Step1 />, <Step2 />, <Step3 />];
-  const { currentStep, isFirstStep, isLastStep, nextStep, prevStep, resetStep } = useStep({ max: steps.length });
-  const CurrentStepComponent = steps[currentStep];
+  const { currentStep, isFirst, isLast, next, back, reset } = useStep(steps.length);
+  const CurrentStepComponent = steps[currentStep - 1];
 
   return (
     <>
-      <p>Is First Step – {isFirstStep}</p>
-      <p>Is Last Step – {isLastStep}</p>
-      <CurrentStepComponent />
-      <button
-        disabled={isLastStep}
-        onClick={nextStep}
-      >
+      <p>Current Step Value – {currentStep}</p>
+      <p>Is First Step – {isFirst}</p>
+      <p>Is Last Step – {isLast}</p>
+      {CurrentStepComponent}
+      <button type='button' disabled={isLast} onClick={next}>
         To Next Step
       </button>
-      <button
-        disabled={isFirstStep}
-        onClick={prevStep}
-      >
+      <button type='button' disabled={isFirst} onClick={back}>
         To Previous Step
       </button>
-      <button
-        disabled={isFirstStep}
-        onClick={resetStep}
-      >
+      <button type='button' disabled={isFirst} onClick={reset}>
         To Initial Step
       </button>
     </>
