aff7f5e1ee376ecfe3172b23d3c65b4014641426
Aws <exibp@mail.ru>
2024-05-28 14:46:01 +0700

    fixed useRerender tests

--- src/hooks/useRerender/useRerender.test.ts
+++ src/hooks/useRerender/useRerender.test.ts
@@ -1,30 +1,17 @@
-import { act } from 'react';
-import { renderHook } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react';
 
 import { useRerender } from './useRerender';
 
-it('Should initialize with id 0', () => {
-  const { result } = renderHook(() => useRerender());
+it('Should use rerender', () => {
+  const { result } = renderHook(useRerender);
 
-  expect(result.current.id).toBe(0);
+  expect(typeof result.current.id).toBe('string');
 });
 
 it('Should update id on update call', () => {
-  const { result } = renderHook(() => useRerender());
+  const { result } = renderHook(useRerender);
   const initialId = result.current.id;
 
   act(() => result.current.update());
   expect(result.current.id).not.toBe(initialId);
-});
-
-it('Should update id to a different value on multiple update calls', () => {
-  const { result } = renderHook(() => useRerender());
-  const initialId = result.current.id;
-
-  act(() => result.current.update());
-  const firstUpdateId = result.current.id;
-  expect(result.current.id).not.toBe(initialId);
-
-  act(() => result.current.update());
-  expect(result.current.id).not.toBe(firstUpdateId);
 });
