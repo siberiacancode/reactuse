fe1eb1681ffa41eb648c654e5563e29328cb4980
Nikita Dubyk <nikita_dubyk@mail.ru>
2024-05-30 14:43:07 +0300

    feat(doc): add jsdoc for hook useDebouncedValue

--- src/hooks/useDebouncedValue/useDebouncedValue.ts
+++ src/hooks/useDebouncedValue/useDebouncedValue.ts
@@ -6,14 +6,35 @@ import { debounce } from '@/utils/helpers';
 // усложняем ли хуки value и state
 
 interface UseDebouncedValueOptions {
+  /** The maximum time the debounce function will wait before being invoked */
   maxWait?: number;
 }
 
+export type UseDocumentTitleReturn<Value> = [
+  /** The debounced state value */
+  state: Value,
+
+  /** Function to update the state value */
+  debouncedSetState: (value: Value) => void
+];
+
+/**
+ * @name useDebouncedValue
+ * @description - Hook that returns a debounced value and a function to update it
+ *
+ * @param {Value} value - The value to be debounced
+ * @param {number} delay - The delay in milliseconds for debouncing
+ * @param {UseDebouncedValueOptions} [options] - Optional configurations for debouncing
+ * @returns {UseDocumentTitleReturn} - Returns the debounced value and a function to update the state
+ *
+ * @example
+ * const [debouncedValue, setDebouncedValue] = useDebouncedValue(value, 300);
+ */
 export const useDebouncedValue = <Value>(
   value: Value,
   delay: number,
   options?: UseDebouncedValueOptions
-) => {
+): UseDocumentTitleReturn<Value> => {
   console.log('@', options);
   const previousValueRef = React.useRef(value);
   const [state, setState] = React.useState(value);
