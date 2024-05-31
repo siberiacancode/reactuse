4b0e034b6bb20df435209c159cdbe5ccc55467a3
debabin <dmitriybabinup@gmail.com>
2024-06-01 00:29:27 +0700

    main 🧊 rework use favicon

--- src/hooks/useFavicon/useFavicon.test.ts
+++ src/hooks/useFavicon/useFavicon.test.ts
@@ -2,8 +2,9 @@ import { act, renderHook } from '@testing-library/react';
 
 import { useFavicon } from './useFavicon';
 
-beforeEach(() => {
-  document.head.innerHTML = '';
+afterEach(() => {
+  const favicon = document.querySelector("link[rel*='icon']");
+  if (favicon) favicon.remove();
 });
 
 it('Should use favicon', () => {
@@ -15,6 +16,21 @@ it('Should use favicon', () => {
 
 it('Should be set initial favicon', () => {
   const { result } = renderHook(() => useFavicon('https://www.google.com/favicon.ico'));
+
+  const favicon = document.querySelector("link[rel*='icon']");
+  expect(favicon).toBeInstanceOf(HTMLLinkElement);
+  expect(result.current.href).toBe('https://www.google.com/favicon.ico');
+});
+
+it('Should set existing favicon', () => {
+  const link = document.createElement('link');
+  link.rel = 'icon';
+  link.href = 'https://www.google.com/favicon.ico';
+  link.type = 'image/ico';
+  document.head.appendChild(link);
+
+  const { result } = renderHook(() => useFavicon());
+
   expect(result.current.href).toBe('https://www.google.com/favicon.ico');
 });
 
@@ -22,16 +38,18 @@ it('Should be set the new favicon', () => {
   const { result } = renderHook(useFavicon);
 
   act(() => result.current.set('https://www.google.com/favicon.ico'));
+
+  const favicon = document.querySelector("link[rel*='icon']");
+  expect(favicon).toBeInstanceOf(HTMLLinkElement);
   expect(result.current.href).toBe('https://www.google.com/favicon.ico');
 });
 
-it('Should be set initial favicon and new options.rel ', () => {
-  const { result } = renderHook(() =>
-    useFavicon('https://www.google.com/favicon.ico', { rel: 'shortcut icon' })
-  );
+it('Should update the favicon when the href changes', () => {
+  const { result, rerender } = renderHook((state) => useFavicon(state), {
+    initialProps: 'https://www.google.com/favicon.ico'
+  });
 
   expect(result.current.href).toBe('https://www.google.com/favicon.ico');
-  expect(document.querySelector<HTMLLinkElement>(`link[href="${result.current.href}"]`)?.rel).toBe(
-    'shortcut icon'
-  );
+  rerender('https://react.dev/favicon-32x32.png');
+  expect(result.current.href).toBe('https://react.dev/favicon-32x32.png');
 });
