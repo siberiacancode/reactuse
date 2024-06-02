dc0c1b5fa419806a918156a3945dc5a0121cb652
debabin <dmitriybabinup@gmail.com>
2024-06-03 01:36:29 +0700

    main 🧊 rework use online test

--- src/hooks/useOnline/useOnline.test.ts
+++ src/hooks/useOnline/useOnline.test.ts
@@ -1,22 +1,24 @@
 import { act, renderHook } from '@testing-library/react';
 
+import { renderHookServer } from '@/tests';
+
 import { useOnline } from './useOnline';
 
 const mockNavigatorOnline = vi.spyOn(navigator, 'onLine', 'get');
 
-it('Should return true when online', () => {
+it('Should use online', () => {
   mockNavigatorOnline.mockReturnValue(true);
   const { result } = renderHook(useOnline);
   expect(result.current).toBeTruthy();
 });
 
-it('Should return false when offline', () => {
-  mockNavigatorOnline.mockReturnValue(false);
-  const { result } = renderHook(useOnline);
+it('Should use network on server', () => {
+  const { result } = renderHookServer(useOnline);
+
   expect(result.current).toBeFalsy();
 });
 
-it('Should change return value upon network events', () => {
+it('Should change value upon network events', () => {
   mockNavigatorOnline.mockReturnValue(true);
   const { result } = renderHook(useOnline);
   expect(result.current).toBeTruthy();
