4e02d6300a31493312bab704f67085957ef85d53
Dmitry Belkin <dmitriy.belkin.2001@mail.ru>
2024-06-02 20:48:49 +0300

    refactor: fix jsdoc

--- src/hooks/usePreferredLanguages/usePreferredLanguages.ts
+++ src/hooks/usePreferredLanguages/usePreferredLanguages.ts
@@ -3,11 +3,13 @@ import React from 'react';
 import { isClient } from '@/utils/helpers';
 
 /**
- * Hook returning the current browser preferred languages.
+ * @name usePreferredLanguages
+ * @description Hook that returns a browser preferred languages from navigator.
+ *
+ * @returns {string[]} Readonly array of strings representing the user's preferred languages
  *
  * @example
  * const languages = usePreferredLanguages()
- *
  */
 export const usePreferredLanguages = () => {
   const preferredLanguages = React.useRef<readonly string[]>([]);
