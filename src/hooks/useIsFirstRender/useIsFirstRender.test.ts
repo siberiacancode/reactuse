e4320875fadafeae939bb1c78cadf0497ae2f1eb
debabin <dmitriybabinup@gmail.com>
2024-06-02 19:08:45 +0700

    main 🧊 rework test use is first render

--- src/hooks/useIsFirstRender/useIsFirstRender.test.ts
+++ src/hooks/useIsFirstRender/useIsFirstRender.test.ts
@@ -2,13 +2,13 @@ import { renderHook } from '@testing-library/react';
 
 import { useIsFirstRender } from './useIsFirstRender';
 
-it('Should return true on the first render', () => {
+it('Should use is first render', () => {
   const { result } = renderHook(useIsFirstRender);
 
   expect(result.current).toBe(true);
 });
 
-it('Should return false on subsequent renders', () => {
+it('Should return false after render', () => {
   const { result, rerender } = renderHook(useIsFirstRender);
 
   expect(result.current).toBe(true);
@@ -18,18 +18,4 @@ it('Should return false on subsequent renders', () => {
 
   rerender();
   expect(result.current).toBe(false);
-});
-
-it('Should maintain state across multiple components', () => {
-  const { result: result1, rerender: rerender1 } = renderHook(useIsFirstRender);
-  const { result: result2, rerender: rerender2 } = renderHook(useIsFirstRender);
-
-  expect(result1.current).toBe(true);
-  expect(result2.current).toBe(true);
-
-  rerender1();
-  expect(result1.current).toBe(false);
-
-  rerender2();
-  expect(result2.current).toBe(false);
 });
