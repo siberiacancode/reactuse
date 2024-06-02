f54146c70649b1e0b0c478722b420602732933d4
Dmytro <dima06444@gmail.com>
2024-06-02 10:51:19 +0300

    chore: add jsdoc

--- src/hooks/useUpdateEffect.ts
+++ src/hooks/useUpdateEffect.ts
@@ -1,5 +1,17 @@
 import { useEffect, useRef } from 'react';
 
+/**
+ * @name useUpdateEffect
+ * @description - Hook that runs an effect only on subsequent renders
+ *
+ * @param {EffectCallback} effect The effect function to run
+ * @param {DependencyList} dependencies The dependencies to watch for changes
+ *
+ * @example
+ * useUpdateEffect(() => {
+ *   // Code to run on subsequent renders
+ * }, [dependencies]);
+ */
 export const useUpdateEffect: typeof useEffect = (effect, dependencies) => {
   const isMounted = useRef(false);
 
