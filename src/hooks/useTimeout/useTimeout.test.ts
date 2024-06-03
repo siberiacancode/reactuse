8cf366e1f04988098129d6d9cd40fe822a2a2f17
Nikita <warface212@gmail.com>
2024-06-03 19:54:40 +0300

    [test]: useTimeout

--- src/hooks/useTimeout/useTimeout.test.ts
+++ src/hooks/useTimeout/useTimeout.test.ts
@@ -6,26 +6,44 @@ beforeEach(() => {
   vi.useFakeTimers();
 });
 
-it("Shouldn't be ready at the start", () => {
-  const { result } = renderHook(() => useTimeout(() => {}, 5000));
+it('Should not call callback at the start', () => {
+  const callback = vi.fn();
+  const { result } = renderHook(() => useTimeout(callback, 5000));
 
   expect(result.current.ready).toBeFalsy();
+  expect(callback).not.toBeCalled();
 });
 
-it("Shouln't be ready unless the timer has expired", () => {
-  const { result } = renderHook(() => useTimeout(() => {}, 5000));
+it('Shoul not call callback unless the timer has expired', () => {
+  const callback = vi.fn();
+  const { result } = renderHook(() => useTimeout(callback, 5000));
 
   act(() => vi.advanceTimersByTime(4999));
 
   expect(result.current.ready).toBeFalsy();
+  expect(callback).not.toBeCalled();
 });
 
-it('Should be ready after the timer expires', () => {
-  const { result } = renderHook(() => useTimeout(() => {}, 5000));
+it('Should call callback after the timer expires', () => {
+  const callback = vi.fn();
+  const { result } = renderHook(() => useTimeout(callback, 5000));
 
   act(() => vi.runAllTimers());
 
   expect(result.current.ready).toBeTruthy();
+  expect(callback).toBeCalledTimes(1);
+});
+
+it('Should not call callback after calling the cleaning function', () => {
+  const callback = vi.fn();
+  const { result } = renderHook(() => useTimeout(callback, 5000));
+
+  act(() => {
+    result.current.clear();
+    vi.runAllTimers();
+  });
+
+  expect(callback).not.toBeCalled();
 });
 
 it('Should be ready after calling the cleaning function', () => {
