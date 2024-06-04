bcc56b33f51b3696193d603f597872243b37c852
debabin <dmitriybabinup@gmail.com>
2024-06-04 19:28:14 +0700

    main 🧊 rework use preferred languages

--- src/hooks/usePreferredLanguages/usePreferredLanguages.test.ts
+++ src/hooks/usePreferredLanguages/usePreferredLanguages.test.ts
@@ -4,25 +4,25 @@ import { renderHookServer } from '@/tests';
 
 import { usePreferredLanguages } from './usePreferredLanguages';
 
-const MockNavigatorLanguages = vi.spyOn(navigator, 'languages', 'get');
+const mockNavigatorLanguages = vi.spyOn(navigator, 'languages', 'get');
 
-it('Should use languages', () => {
-  MockNavigatorLanguages.mockReturnValue(['en', 'en-US', 'fr', 'fr-FR']);
+it('Should use preferred languages', () => {
+  mockNavigatorLanguages.mockReturnValue(['en', 'en-US', 'fr', 'fr-FR']);
   const { result } = renderHook(usePreferredLanguages);
   expect(result.current).toEqual(['en', 'en-US', 'fr', 'fr-FR']);
 });
 
-it('Should use languages on server', () => {
+it('Should use preferred languages on server', () => {
   const { result } = renderHookServer(usePreferredLanguages);
   expect(result.current).toEqual(['en']);
 });
 
 it('Should change value upon language changes', () => {
-  MockNavigatorLanguages.mockReturnValue(['en', 'en-US', 'fr', 'fr-FR']);
+  mockNavigatorLanguages.mockReturnValue(['en', 'en-US', 'fr', 'fr-FR']);
   const { result } = renderHook(usePreferredLanguages);
   expect(result.current).toEqual(['en', 'en-US', 'fr', 'fr-FR']);
 
-  MockNavigatorLanguages.mockReturnValue(['en', 'en-US', 'fr', 'fr-FR', 'de']);
+  mockNavigatorLanguages.mockReturnValue(['en', 'en-US', 'fr', 'fr-FR', 'de']);
   act(() => window.dispatchEvent(new Event('languagechange')));
   expect(result.current).toEqual(['en', 'en-US', 'fr', 'fr-FR', 'de']);
 });
