b8eb391a2288615ea33ebb283a13b6c31b8e3d2b
VladPorvin <porvinvlad.2019@gmail.com>
2024-06-08 01:43:03 +0300

    Refactor usePaint hook

--- src/hooks/usePaint/usePaint.demo.tsx
+++ src/hooks/usePaint/usePaint.demo.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import { usePaint } from './usePaint';
 
 const Demo = () => {
@@ -9,7 +11,25 @@ const Demo = () => {
     setPencilWidth,
     setPencilOpacity,
     setPencilColor,
-    clearCanvas
+    clearCanvas,
+    pencilColor,
+    pencilWidth,
+    pencilOpacity
+  } = usePaint({
+    pencilOptions: { width: 10, color: '#e63946', opacity: 0.1 },
+    canvasOptions: {
+      bgColor: '#fff',
+      width: 400,
+      height: 300
+    }
+  });
+
+  const {
+    canvasRef: canvasRef2,
+    startDrawing: startDrawing2,
+    endDrawing: endDrawing2,
+    drawLine: drawLine2,
+    clearCanvas: clearCanvas2
   } = usePaint({
     pencilOptions: { width: 10, color: '#e63946' },
     canvasOptions: {
@@ -19,12 +39,42 @@ const Demo = () => {
     }
   });
 
+  const clearCanvasHandler = (event: KeyboardEvent) => {
+    if (event.key === 'c') {
+      clearCanvas2();
+    }
+  };
+
+  React.useEffect(() => {
+    const canvas = canvasRef2.current;
+    if (!canvas) return;
+
+    window.addEventListener('keydown', clearCanvasHandler);
+
+    return () => {
+      window.removeEventListener('keydown', clearCanvasHandler);
+    };
+  }, [canvasRef]);
+
+  const downloadImage = () => {
+    const canvas = canvasRef.current;
+
+    if (!canvas) return;
+
+    const image = canvas.toDataURL();
+    const link = document.createElement('a');
+    link.href = image;
+    link.download = 'canvas.png';
+    link.click();
+  };
+
   return (
     <div>
       <div style={{ display: 'flex', flexDirection: 'column' }}>
         <div>
           <label>Brush Color</label>
           <input
+            value={pencilColor}
             type='color'
             onChange={(event) => {
               setPencilColor(event.target.value);
@@ -34,6 +84,7 @@ const Demo = () => {
         <div>
           <label>Brush Width</label>
           <input
+            value={pencilWidth}
             type='range'
             min='3'
             max='20'
@@ -45,11 +96,13 @@ const Demo = () => {
         <div>
           <label>Brush Opacity</label>
           <input
+            value={pencilOpacity}
             type='range'
-            min='1'
-            max='100'
+            min='0'
+            max='1'
+            step='0.01'
             onChange={(event) => {
-              setPencilOpacity(Number(event.target.value) / 100);
+              setPencilOpacity(Number(event.target.value));
             }}
           />
         </div>
@@ -57,12 +110,27 @@ const Demo = () => {
       <button type='button' onClick={clearCanvas}>
         Clear
       </button>
-      <p>Paint here</p>
+      <button type='button' onClick={downloadImage}>
+        Download image
+      </button>
+      <p>Pencil color: {pencilColor}</p>
+      <p>Pencil width: {pencilWidth}</p>
+      <p>Pencil opacity: {pencilOpacity}</p>
       <canvas
         onMouseDown={startDrawing}
         onMouseUp={endDrawing}
         onMouseMove={drawLine}
         ref={canvasRef}
+      />
+
+      <hr />
+
+      <p>Press button &apos;c&apos; to clear</p>
+      <canvas
+        onMouseDown={startDrawing2}
+        onMouseUp={endDrawing2}
+        onMouseMove={drawLine2}
+        ref={canvasRef2}
       />
     </div>
   );
--- src/hooks/usePaint/usePaint.ts
+++ src/hooks/usePaint/usePaint.ts
@@ -1,13 +1,27 @@
 import React from 'react';
 
 export interface UsePaintReturn {
+  /** Canvas ref */
   canvasRef: React.RefObject<HTMLCanvasElement>;
+  /** Function to start the drawing */
   startDrawing: (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => void;
+  /** Function to end the drawing */
   endDrawing: () => void;
+  /** Function to draw a line */
   drawLine: (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => void;
+  /** Function to set the pencil width */
   setPencilWidth: (width: number) => void;
+  /** Pencil width */
+  pencilWidth: number;
+  /** Function to set the pencil opacity */
   setPencilOpacity: (opacity: number) => void;
-  setPencilColor: (width: string | CanvasGradient | CanvasPattern) => void;
+  /** Pencil opacity */
+  pencilOpacity: number;
+  /** Function to set the pencil color */
+  setPencilColor: (width: string) => void;
+  /** Pencil color */
+  pencilColor: string;
+  /** Function to clear the canvas */
   clearCanvas: () => void;
 }
 
@@ -17,14 +31,21 @@ export interface UsePaintParams {
 }
 
 export type CanvasOptions = {
+  /** Canvas width */
   width?: number;
+  /** Canvas height */
   height?: number;
+  /** Canvas background color */
   bgColor?: string;
 };
 
 export type PencilOptions = {
+  /** Pencil color */
   color?: string;
+  /** Pencil width */
   width?: number;
+  /** Pencil opacity */
+  opacity?: number;
 };
 
 const defaultWidth = 200;
@@ -44,7 +65,8 @@ const defaultHeight = defaultWidth;
  * @param {PencilOptions} options.pencilOptions - Optional properties for customizing the pencil.
  * @param {string} options.pencilOptions.color - Optional color of the pencil. Defaults to '#000'.
  * @param {number} options.pencilOptions.width - Optional width of the pencil. Defaults to 5.
- * @returns {UsePaintReturn} - Object containing the canvas reference, function to set the pencil color, and the currently selected pencil color.
+ * @param {number} options.pencilOptions.opacity - Optional opacity of the pencil. Defaults to 0.1.
+ * @returns {UsePaintReturn} - Object containing the canvas reference, function to set the pencil color, pencil width, pencil opacity and function to clear the canvas.
  * 
  * @example
  * const {
@@ -65,43 +87,27 @@ const defaultHeight = defaultWidth;
  */
 
 export const usePaint = (options: UsePaintParams = {}): UsePaintReturn => {
-  const canvasRef = React.useRef<HTMLCanvasElement>(null);
+  const canvasRef = React.useRef<HTMLCanvasElement | null>(null);
   const ctxRef = React.useRef<CanvasRenderingContext2D | null>(null);
+
   const [isDrawing, setIsDrawing] = React.useState(false);
   const [lineWidth, setLineWidth] = React.useState(options?.pencilOptions?.width || 5);
-  const [lineColor, setLineColor] = React.useState<string | CanvasGradient | CanvasPattern>(
+  const [lineColor, setLineColor] = React.useState<string>(
     options?.pencilOptions?.color || 'black'
   );
-  const [lineOpacity, setLineOpacity] = React.useState(0.1);
+  const [lineOpacity, setLineOpacity] = React.useState(options?.pencilOptions?.opacity || 0.1);
 
   const setPencilWidth = (width: number) => {
     setLineWidth(width);
   };
 
-  const setPencilColor = (color: string | CanvasGradient | CanvasPattern) => {
+  const setPencilColor = (color: string) => {
     setLineColor(color);
   };
 
   const setPencilOpacity = (opacity: number) => {
     setLineOpacity(opacity);
   };
-
-  React.useEffect(() => {
-    const canvas = canvasRef.current;
-
-    if (!canvas) return;
-
-    const ctx = canvas.getContext('2d');
-
-    if (!ctx) return;
-
-    ctx.lineCap = 'round';
-    ctx.lineJoin = 'round';
-    ctx.globalAlpha = lineOpacity;
-    ctx.strokeStyle = lineColor;
-    ctx.lineWidth = lineWidth;
-    ctxRef.current = ctx;
-  }, [lineColor, lineOpacity, lineWidth]);
 
   const startDrawing = (event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
     if (!ctxRef.current) return;
@@ -144,6 +150,7 @@ export const usePaint = (options: UsePaintParams = {}): UsePaintReturn => {
 
     canvas.width = options?.canvasOptions?.width || defaultWidth;
     canvas.height = options?.canvasOptions?.height || defaultHeight;
+
     const ctx = canvas.getContext('2d');
     if (!ctx) return;
 
@@ -155,6 +162,21 @@ export const usePaint = (options: UsePaintParams = {}): UsePaintReturn => {
     options?.canvasOptions?.width
   ]);
 
+  React.useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const ctx = canvas.getContext('2d');
+    if (!ctx) return;
+
+    ctx.lineCap = 'round';
+    ctx.lineJoin = 'round';
+    ctx.globalAlpha = lineOpacity;
+    ctx.strokeStyle = lineColor;
+    ctx.lineWidth = lineWidth;
+    ctxRef.current = ctx;
+  }, [lineColor, lineOpacity, lineWidth]);
+
   return {
     canvasRef,
     startDrawing,
@@ -162,6 +184,9 @@ export const usePaint = (options: UsePaintParams = {}): UsePaintReturn => {
     drawLine,
     setPencilWidth,
     setPencilColor,
+    pencilColor: lineColor,
+    pencilWidth: lineWidth,
+    pencilOpacity: lineOpacity,
     setPencilOpacity,
     clearCanvas
   };
