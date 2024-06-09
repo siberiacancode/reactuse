cb7361521bf6bdbba6b5fa6d5d227567a8357e45
debabin <dmitriybabinup@gmail.com>
2024-06-09 23:10:13 +0700

    main 🧊 rework use window size tests

--- src/hooks/useWindowSize/useWindowSize.test.ts
+++ src/hooks/useWindowSize/useWindowSize.test.ts
@@ -2,21 +2,28 @@ import { act, renderHook } from '@testing-library/react';
 
 import { useWindowSize } from './useWindowSize';
 
+const mockWindowInnerHeight = vi.spyOn(window, 'innerHeight', 'get');
+const mockWindowInnerWidth = vi.spyOn(window, 'innerWidth', 'get');
+
 it('Should use window size', () => {
-  Object.assign(window, { innerHeight: 100, innerWidth: 100 });
+  mockWindowInnerHeight.mockReturnValue(100);
+  mockWindowInnerWidth.mockReturnValue(100);
+
   const { result } = renderHook(useWindowSize);
 
   expect(result.current).toEqual({ width: 100, height: 100 });
 });
 
 it('Should change state upon resize events', () => {
-  Object.assign(window, { innerHeight: 100, innerWidth: 100 });
+  mockWindowInnerHeight.mockReturnValue(100);
+  mockWindowInnerWidth.mockReturnValue(100);
   const { result } = renderHook(useWindowSize);
 
   expect(result.current).toEqual({ width: 100, height: 100 });
 
   act(() => {
-    Object.assign(window, { innerHeight: 50, innerWidth: 50 });
+    mockWindowInnerHeight.mockReturnValue(50);
+    mockWindowInnerWidth.mockReturnValue(50);
     window.dispatchEvent(new Event('resize'));
   });
 
