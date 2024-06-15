faab5b4c29e1e537655939c0869c4781e3b90459
debabin <dmitriybabinup@gmail.com>
2024-06-15 15:53:42 +0700

    main 🧊 rework use operating system

--- src/hooks/useOperatingSystem/useOperatingSystem.test.ts
+++ src/hooks/useOperatingSystem/useOperatingSystem.test.ts
@@ -2,27 +2,27 @@ import { renderHook } from '@testing-library/react';
 
 import { useOperatingSystem } from './useOperatingSystem';
 
+const mockNavigatorUserAgent = vi.spyOn(navigator, 'userAgent', 'get');
+
 it('Should use operating system', () => {
   const { result } = renderHook(useOperatingSystem);
   expect(typeof result.current).toBe('string');
 });
 
-const userAgentAndExpectedOSs = [
-  { userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)', expectedOS: 'macos' },
-  { userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_7_1 like Mac OS X)', expectedOS: 'ios' },
-  { userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)', expectedOS: 'windows' },
-  { userAgent: 'Mozilla/5.0 (Linux; Android 11; Pixel 5)', expectedOS: 'android' },
-  { userAgent: 'Mozilla/5.0 (X11; Linux x86_64)', expectedOS: 'linux' },
-  { userAgent: 'Mozilla/5.0 (Unknown)', expectedOS: 'undetermined' }
+const USER_AGENTS = [
+  { userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)', operatingSystem: 'macos' },
+  { userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_7_1 like Mac OS X)', operatingSystem: 'ios' },
+  { userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)', operatingSystem: 'windows' },
+  { userAgent: 'Mozilla/5.0 (Linux; Android 11; Pixel 5)', operatingSystem: 'android' },
+  { userAgent: 'Mozilla/5.0 (X11; Linux x86_64)', operatingSystem: 'linux' },
+  { userAgent: 'Mozilla/5.0 (Unknown)', operatingSystem: 'undetermined' }
 ];
 
-userAgentAndExpectedOSs.forEach((userAgentAndExpectedOS) => {
-  it(`Should return ${userAgentAndExpectedOS.expectedOS} for user agent: ${userAgentAndExpectedOS.userAgent}`, () => {
-    vi.spyOn(navigator, 'userAgent', 'get').mockImplementation(
-      () => userAgentAndExpectedOS.userAgent
-    );
+USER_AGENTS.forEach(({ userAgent, operatingSystem }) => {
+  it(`Should return ${operatingSystem} for user agent: ${userAgent}`, () => {
+    mockNavigatorUserAgent.mockReturnValue(userAgent);
 
     const { result } = renderHook(useOperatingSystem);
-    expect(result.current).toBe(userAgentAndExpectedOS.expectedOS);
+    expect(result.current).toBe(operatingSystem);
   });
 });
