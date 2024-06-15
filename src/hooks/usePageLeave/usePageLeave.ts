32e53c53164174a1fcc6b73e1b9280ce37b5aa97
debabin <dmitriybabinup@gmail.com>
2024-06-15 17:29:54 +0700

    main 🧊 rework use page leave

--- src/hooks/usePageLeave/usePageLeave.ts
+++ src/hooks/usePageLeave/usePageLeave.ts
@@ -1,4 +1,6 @@
-import { useEventListener } from '@/hooks';
+import React from 'react';
+
+import { useEvent } from '../useEvent/useEvent';
 
 /**
  * @name usePageLeave
@@ -9,5 +11,24 @@ import { useEventListener } from '@/hooks';
  * @example
  * usePageLeave(() => console.log('on leave'))
  */
-export const usePageLeave = (callback: () => void) =>
-  useEventListener(document, 'mouseleave', callback);
+export const usePageLeave = (callback?: () => void) => {
+  const [isLeft, setIsLeft] = React.useState(false);
+
+  const onMouse = useEvent(() => {
+    if (isLeft) return setIsLeft(false);
+    callback?.();
+    setIsLeft(true);
+  });
+
+  React.useEffect(() => {
+    document.addEventListener('mouseleave', onMouse, { passive: true });
+    document.addEventListener('mouseenter', onMouse, { passive: true });
+
+    return () => {
+      document.removeEventListener('mouseenter', onMouse);
+      document.removeEventListener('mouseleave', onMouse);
+    };
+  }, []);
+
+  return isLeft;
+};
