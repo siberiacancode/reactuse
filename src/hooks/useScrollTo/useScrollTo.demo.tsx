5e94b441879c4b90e5004ebb80a265d97ec66864
Nikita <94167692+zeroqs@users.noreply.github.com>
2024-06-26 01:33:04 +0300

    fix: fix style naming

--- src/hooks/useScrollTo/useScrollTo.demo.tsx
+++ src/hooks/useScrollTo/useScrollTo.demo.tsx
@@ -1,6 +1,6 @@
 import { useScrollTo } from "@/hooks/useScrollTo/useScrollTo";
 
-const cardStyle = {
+const blockStyle = {
     border: '1px solid gray',
     height: 300,
     width: 300,
@@ -20,9 +20,9 @@ const Demo = () => {
         <>
             <button onClick={handleClick}>Scroll to block 3</button>
             <div style={{overflow: 'auto', height: '320px', position: 'relative' }}>
-                <div style={cardStyle}>Block 1</div>
-                <div style={cardStyle}>Block 2</div>
-                <div ref={targetToScroll} style={cardStyle}>Block 3</div>
+                <div style={blockStyle}>Block 1</div>
+                <div style={blockStyle}>Block 2</div>
+                <div ref={targetToScroll} style={blockStyle}>Block 3</div>
         </div>
         </>
     )
