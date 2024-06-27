d2841861cec07b5ec3aaf23f081e503b1aa6319d
hywax <me@hywax.space>
2024-06-27 22:14:29 +0500

    fix: useBattery change category

--- src/hooks/useBattery/useBattery.ts
+++ src/hooks/useBattery/useBattery.ts
@@ -27,7 +27,7 @@ interface UseBatteryStateReturn {
 /**
  * @name useBattery
  * @description - Hook for getting information about battery status
- * @category Sensors
+ * @category Browser
  *
  * @returns {UseBatteryStateReturn} Object containing battery information & Battery API support
  *
