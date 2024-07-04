c4f135424ef69d656125efead41fbc5c33c7f222
debabin <dmitriybabinup@gmail.com>
2024-07-04 22:55:20 +0700

    main 🧊 add readme for preferred hooks

--- src/hooks/usePreferredContrast/usePreferredContrast.ts
+++ src/hooks/usePreferredContrast/usePreferredContrast.ts
@@ -1,6 +1,19 @@
 import { useMediaQuery } from '../useMediaQuery/useMediaQuery';
 
-export const usePreferredContrast = () => {
+/** The use preferred contrast return type */
+export type UsePreferredContrastReturn = 'more' | 'less' | 'custom' | 'no-preference';
+
+/**
+ * @name usePreferredContrast
+ * @description - Hook that returns the contrast preference
+ * @category Browser
+ *
+ * @returns {UsePreferredContrastReturn} The contrast preference
+ *
+ * @example
+ * const contrast = usePreferredContrast();
+ */
+export const usePreferredContrast = (): UsePreferredContrastReturn => {
   const more = useMediaQuery('(prefers-contrast: more)');
   const less = useMediaQuery('(prefers-contrast: less)');
   const custom = useMediaQuery('(prefers-contrast: custom)');
