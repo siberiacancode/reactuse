b24ef908c65cd3166d05eb7a18c5e0426f6e52e4
VLADISLAW9 <duplinskij_v_d@mail.ru>
2024-09-18 20:09:51 +0700

    #243 CODE REVIEW: remove UseLastChangedSource type, remove if (source) in useEffect, change "getDate().timestamp" to Date.now()

--- src/hooks/useLastChanged/useLastChanged.ts
+++ src/hooks/useLastChanged/useLastChanged.ts
@@ -1,10 +1,5 @@
 import { useEffect, useState } from 'react';
 
-import { getDate } from '@/utils/helpers';
-
-/** The use last changed source type */
-export type UseLastChangedSource<T> = T;
-
 /** The use last changed options type  */
 export interface UseLastChangedOptions {
   initialValue?: number;
@@ -15,23 +10,18 @@ export interface UseLastChangedOptions {
  * @description - Hook for records the timestamp of the last change
  * @category Time
  *
- * @param {UseLastChangedSource} source  The source of the last change
+ * @param {any} source  The source of the last change
  * @param {number | null} [options.initialValue=null] The initial value
  * @returns {number | null} Return timestamp of the last change
  *
  * @example
- * const lastChanged = useLastChanged(source, { initialValue: 0 });
+ * const lastChanged = useLastChanged(source);
  */
-export const useLastChanged = <T>(
-  source: UseLastChangedSource<T>,
-  options?: UseLastChangedOptions
-): number | null => {
+export const useLastChanged = (source: any, options?: UseLastChangedOptions): number | null => {
   const [lastChanged, setLastChanged] = useState(options?.initialValue ?? null);
 
   useEffect(() => {
-    if (source) {
-      setLastChanged(getDate().timestamp);
-    }
+    setLastChanged(Date.now());
   }, [source]);
 
   return lastChanged;
