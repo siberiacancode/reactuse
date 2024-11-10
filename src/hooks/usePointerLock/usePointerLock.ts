21738a42d50b6f3bdc52ee804ddf8ddd79eea7eb
VLADISLAW9 <duplinskij_v_d@mail.ru>
2024-11-10 20:44:24 +0700

    #244 CODE REVIEW: перенес хендлеры в useEffect, поправил jsdoc хука

--- src/hooks/usePointerLock/usePointerLock.ts
+++ src/hooks/usePointerLock/usePointerLock.ts
@@ -12,11 +12,11 @@ interface UsePointerLockReturn {
 
 /**
  * @name usePointerLock
- * @description - Reactive pointer lock
+ * @description Hook that provides reactive pointer lock
  * @category Sensors
  *
- * @param {RefObject<Element>} target
- * @returns {UsePointerLockReturn} An object containing the current pointer lock element and functions to interact with the pointer lock
+ * @param {UsePointerLockTarget} target The target element for the pointer lock
+ * @returns {UsePointerLockReturn} An object containing the pointer lock element and functions to interact with the pointer lock
  *
  * @example
  * const { supported, lock, unlock, element } = usePointerLock();
@@ -27,29 +27,29 @@ export const usePointerLock = (target?: UsePointerLockTarget): UsePointerLockRet
 
   const [element, setElement] = useState<Element>();
 
-  const handlePointerLockChange = () => {
-    if (!supported) return;
-
-    const currentElement = document.pointerLockElement ?? element;
-
-    if (currentElement && currentElement === element) {
-      setElement(document.pointerLockElement as Element);
-    }
-  };
-  const handlePointerLockError = () => {
-    if (!supported) return;
-
-    const currentElement = document.pointerLockElement ?? element;
-
-    if (currentElement && currentElement === element) {
-      const action = document.pointerLockElement ? 'release' : 'acquire';
-
-      throw new Error(`Failed to ${action} pointer lock.`);
-    }
-  };
-
   useEffect(() => {
     if (!supported) return;
+
+    const handlePointerLockChange = () => {
+      if (!supported) return;
+
+      const currentElement = document.pointerLockElement ?? element;
+
+      if (currentElement && currentElement === element) {
+        setElement(document.pointerLockElement as Element);
+      }
+    };
+    const handlePointerLockError = () => {
+      if (!supported) return;
+
+      const currentElement = document.pointerLockElement ?? element;
+
+      if (currentElement && currentElement === element) {
+        const action = document.pointerLockElement ? 'release' : 'acquire';
+
+        throw new Error(`Failed to ${action} pointer lock.`);
+      }
+    };
 
     document.addEventListener('pointerlockchange', handlePointerLockChange);
     document.addEventListener('pointerlockerror', handlePointerLockError);
@@ -63,13 +63,13 @@ export const usePointerLock = (target?: UsePointerLockTarget): UsePointerLockRet
   const lock = (event: MouseEvent<Element> | Event) => {
     if (!supported) return;
 
-    const targetElement = event instanceof Event ? target?.current : event.currentTarget;
+    const element = event instanceof Event ? target?.current : event.currentTarget;
 
-    if (!targetElement) return;
+    if (!element) return;
 
-    targetElement.requestPointerLock();
+    element.requestPointerLock();
 
-    setElement(targetElement);
+    setElement(element);
   };
 
   const unlock = () => {
