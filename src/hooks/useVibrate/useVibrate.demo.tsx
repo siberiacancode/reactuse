e67e0d6a13f79b6528bfdfa31252551269af16ca
debabin <dmitriybabinup@gmail.com>
2024-12-22 15:42:57 +0700

    main 🧊 add title for demo

--- src/hooks/useVibrate/useVibrate.demo.tsx
+++ src/hooks/useVibrate/useVibrate.demo.tsx
@@ -5,6 +5,7 @@ const Demo = () => {
 
   return (
     <>
+      <h1>Vibration</h1>
       <p>Most modern mobile devices include vibration hardware, which lets software code provides physical feedback to the user by causing the device to shake.</p>
       <button type='button' onClick={() => vibrate.trigger()}>
         Vibrate
