0e93dc114b7ad2a1a1cd149c119dce55bf285536
VLADISLAW9 <duplinskij_v_d@mail.ru>
2025-01-25 14:14:48 +0700

    попробывал сделать ResizeObserver mock

--- src/hooks/useElementSize/useElementSize.test.ts
+++ src/hooks/useElementSize/useElementSize.test.ts
@@ -2,6 +2,36 @@ import { renderHook } from '@testing-library/react';
 
 import { useElementSize } from './useElementSize';
 
+window.ResizeObserver = vi.fn().mockImplementation((callback: ResizeObserverCallback) => {
+  return {
+    observe: (target: Element) => {
+      const width = Number.parseFloat(getComputedStyle(target).width || '0');
+      const height = Number.parseFloat(getComputedStyle(target).height || '0');
+      callback(
+        [
+          {
+            target,
+            contentRect: {
+              width,
+              height,
+              top: 0,
+              left: 0,
+              right: width,
+              bottom: height,
+              x: 0,
+              y: 0
+            },
+            borderBoxSize: [{ inlineSize: width, blockSize: height }]
+          }
+        ] as unknown as ResizeObserverEntry[],
+        null as any
+      );
+    },
+    unobserve: vi.fn(),
+    disconnect: vi.fn()
+  };
+});
+
 it('Should use element size', () => {
   const { result } = renderHook(() => useElementSize());
 
@@ -16,3 +46,27 @@ it('Should set initial value', () => {
 
   expect(result.current.value).toEqual({ width: 100, height: 100 });
 });
+
+// it('Should update value', () => {
+//   const div = document.createElement('div');
+
+//   document.body.appendChild(div);
+
+//   const { result } = renderHook(() => useElementSize(div));
+
+//   expect(result.current.value).toEqual({ width: 0, height: 0 });
+
+//   div.style.width = '100px';
+//   div.style.height = '100px';
+
+//   (window.ResizeObserver as any).mock.calls[0][0]([
+//     {
+//       target: div,
+//       contentRect: { width: 100, height: 100 }
+//     }
+//   ]);
+
+//   expect(result.current.value).toEqual({ width: 100, height: 100 });
+
+//   document.body.removeChild(div);
+// });
