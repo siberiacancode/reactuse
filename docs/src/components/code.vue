2136b2939a5f7f9ddbeaec6fad356d8314e99fcc
debabin <dmitriybabinup@gmail.com>
2025-01-31 23:43:44 +0700

    main 🧊 optimize code component

--- docs/src/components/code.vue
+++ docs/src/components/code.vue
@@ -5,18 +5,12 @@ const props = defineProps({
   code: String,
   lang: String
 });
-
-const container = ref(null);
-onMounted(() => {
-  if (container.value) {
-    container.value.innerHTML += props.code;
-  }
-});
 </script>
 
 <template>
-  <div ref="container" :class="`language-${props.lang} vp-adaptive-theme vp-code`">
+  <div :class="`language-${props.lang} vp-adaptive-theme vp-code`">
     <button title="Copy code" class="copy" />
     <span class="lang">{{ props.lang }}</span>
+    <div v-html="props.code" />
   </div>
 </template>
