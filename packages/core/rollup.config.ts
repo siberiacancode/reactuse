939ec435fdcbc8613d7a38f8884c2fa67eca0c0f
debabin <dmitriybabinup@gmail.com>
2025-03-28 23:06:02 +0700

    main 🧊 migrate to vite

--- packages/core/rollup.config.ts
+++
@@ -1,8 +0,0 @@
-import { generateRollupConfig } from '@siberiacancode/builder';
-
-import pkg from './package.json';
-
-export default generateRollupConfig({
-  pkg,
-  input: { ignorePattern: 'src/**/*.{demo,test}.{ts,tsx}' }
-});
