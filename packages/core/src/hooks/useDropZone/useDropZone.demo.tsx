d7c0d315247eeb3e8f885016ef5f7e5a694ecb4e
VLADISLAW9 <duplinskij_v_d@mail.ru>
2025-04-27 18:40:39 +0700

    #317 🧊 formatter

--- packages/core/src/hooks/useDropZone/useDropZone.demo.tsx
+++ packages/core/src/hooks/useDropZone/useDropZone.demo.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { useDropZone } from "./useDropZone";
+import { useState } from 'react';
+import { useDropZone } from './useDropZone';
 
 interface FileMeta {
   name: string;
@@ -21,7 +21,7 @@ const Demo = () => {
         name: file.name,
         size: file.size,
         type: file.type,
-        lastModified: file.lastModified,
+        lastModified: file.lastModified
       }))
     );
   };
@@ -33,38 +33,32 @@ const Demo = () => {
       <p>Drop files from your computer on to drop zones</p>
       <div
         ref={dropZone.ref}
-        className="flex flex-col p-5 w-full min-h-[300px] bg-gray-400/10 mt-6 rounded"
+        className='flex flex-col p-5 w-full min-h-[300px] bg-gray-400/10 mt-6 rounded'
       >
-        <div className="m-auto">
-          <p className="text-xl font-bold">Drop Zone</p>
+        <div className='m-auto'>
+          <p className='text-xl font-bold'>Drop Zone</p>
           <p>
-            isOver:{" "}
-            <span
-              className={dropZone.isOver ? "text-green-500" : "text-red-500"}
-            >
+            isOver:{' '}
+            <span className={dropZone.isOver ? 'text-green-500' : 'text-red-500'}>
               {String(dropZone.isOver)}
             </span>
           </p>
         </div>
-        <div className="flex flex-col gap-3">
+        <div className='flex flex-col gap-3'>
           {!!files.length &&
             files.map((file, index) => (
-              <div
-                key={index}
-                className="flex p-5 bg-gray-400/5 flex-col rounded"
-              >
+              <div key={index} className='flex p-5 bg-gray-400/5 flex-col rounded'>
                 <p>
-                  <span className="font-bold">File name:</span> {file.name}
+                  <span className='font-bold'>File name:</span> {file.name}
                 </p>
                 <p>
-                  <span className="font-bold">Size:</span> {file.size}
+                  <span className='font-bold'>Size:</span> {file.size}
                 </p>
                 <p>
-                  <span className="font-bold">Type:</span> {file.type}
+                  <span className='font-bold'>Type:</span> {file.type}
                 </p>
                 <p>
-                  <span className="font-bold">Last modified:</span>{" "}
-                  {file.lastModified}
+                  <span className='font-bold'>Last modified:</span> {file.lastModified}
                 </p>
               </div>
             ))}
