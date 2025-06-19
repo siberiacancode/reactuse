fa5e2f7cca9ab8a26b71121f2947491979159ec4
babin <d.babin@cft.ru>
2025-06-19 15:47:03 +0700

    main 🧊 styles for hooks sections changes

--- packages/docs/app/.vitepress/sections/HomeHooks/HomeHooks.vue
+++ packages/docs/app/.vitepress/sections/HomeHooks/HomeHooks.vue
@@ -24,9 +24,12 @@ const filteredHooks = computed(() =>
 </script>
 
 <template>
-  <div class="mb-5 mt-12 text-center text-4xl font-bold">The hooks you need</div>
-  <div class="py-4! px-2! relative h-[325px] overflow-hidden rounded-lg bg-[var(--vp-c-bg-soft)]">
-    <div>
+  <div
+    class="py-8! px-6! relative mt-10 h-[340px] overflow-hidden rounded-lg bg-[var(--vp-c-bg-soft)] lg:h-[365px]"
+  >
+    <div class="flex flex-col items-center justify-between gap-4 md:flex-row">
+      <div class="text-center text-3xl font-bold md:text-4xl xl:text-6xl">The hooks you need</div>
+
       <div
         class="flex w-fit items-center gap-3 rounded-lg bg-[var(--vp-c-default-soft)] p-2 px-4 text-2xl"
       >
@@ -49,14 +52,14 @@ const filteredHooks = computed(() =>
       </div>
     </div>
 
-    <div class="absolute left-2 top-20 flex w-[120%] flex-wrap justify-start gap-3">
+    <div class="md:top-30 absolute left-2 top-40 flex w-[130%] flex-wrap justify-start gap-3">
       <div v-for="hook in filteredHooks.slice(0, 40)" :key="hook.name">
         <a
           :href="`/reactuse/functions/hooks/${hook.name}`"
           class="no-underline! text-[var(--vp-c-text-1)]! text-2xl"
         >
           <div
-            class="hover:border-[var(--vp-c-brand-1)]/80 items-center rounded-lg border-[1px] border-transparent bg-[var(--vp-c-default-soft)] px-6 py-2 transition-all duration-300 hover:shadow-md"
+            class="hover:border-[var(--vp-c-brand-1)]/80 items-center rounded-lg border-[1px] border-transparent bg-[var(--vp-c-default-soft)] px-6 py-2 transition-all duration-200 hover:shadow-md"
             :class="{
               'opacity-50': hook.disabled
             }"
