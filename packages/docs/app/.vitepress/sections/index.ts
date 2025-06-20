254edf82867a7babea2ab11fb77b65a7bcf8b948
babin <d.babin@cft.ru>
2025-06-20 14:24:51 +0700

    main 🧊 add contributors section

--- packages/docs/app/.vitepress/sections/index.ts
+++ packages/docs/app/.vitepress/sections/index.ts
@@ -1,2 +1,3 @@
+export { default as HomeContributors } from './HomeContributors/HomeContributors.vue';
 export { default as HomeHeroBefore } from './HomeHeroBefore/HomeHeroBefore.vue';
 export { default as HomeHooks } from './HomeHooks/HomeHooks.vue';
