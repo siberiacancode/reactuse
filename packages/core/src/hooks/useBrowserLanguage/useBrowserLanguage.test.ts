0f9a1aeea97a2615d58966ada7ce7642f58de060
debabin <dmitriybabinup@gmail.com>
2025-06-27 21:25:57 +0700

    main 🧊 add use browser language test

---
+++ packages/core/src/hooks/useBrowserLanguage/useBrowserLanguage.test.ts
@@ -0,0 +1,41 @@
+import { act, renderHook } from '@testing-library/react';
+
+import { renderHookServer } from '@/tests';
+
+import { useBrowserLanguage } from './useBrowserLanguage';
+
+beforeAll(() => {
+  Object.defineProperty(navigator, 'language', {
+    value: 'ru',
+    writable: true
+  });
+});
+
+it('Should use browser language', () => {
+  const { result } = renderHook(useBrowserLanguage);
+
+  expect(result.current).toBe('ru');
+});
+
+it('Should use browser language on server', () => {
+  const { result } = renderHookServer(useBrowserLanguage);
+
+  expect(result.current).toBe('undetermined');
+});
+
+it('Should handle language change event', () => {
+  const { result } = renderHook(useBrowserLanguage);
+
+  expect(result.current).toBe('ru');
+
+  act(() => {
+    Object.defineProperty(navigator, 'language', {
+      value: 'en',
+      writable: true
+    });
+
+    window.dispatchEvent(new Event('languagechange'));
+  });
+
+  expect(result.current).toBe('en');
+});
