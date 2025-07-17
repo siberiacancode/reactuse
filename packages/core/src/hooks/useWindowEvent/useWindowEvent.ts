d0684c3f717c1e7eb014c0c8c39735d3519e6f22
debabin <dmitriybabinup@gmail.com>
2025-07-17 20:37:02 +0700

    main 🧊 delete category

--- packages/core/src/hooks/useWindowEvent/useWindowEvent.ts
+++ packages/core/src/hooks/useWindowEvent/useWindowEvent.ts
@@ -7,7 +7,7 @@ import { useEventListener } from '../useEventListener/useEventListener';
 /**
  * @name useWindowEvent
  * @description - Hook attaches an event listener to the window object for the specified event
- * @category Events
+ * @category Sensors
  *
  * @template Event Key of window event map.
  * @param {Event} event The event to listen for.
