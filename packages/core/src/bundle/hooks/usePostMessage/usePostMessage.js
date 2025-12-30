d51cc31cb35caacc82a0f1f7cf6c6eab799608f1
iiivanpopov <iiivanpopov999@gmail.com>
2025-12-30 19:27:54 +0200

    main 🧊 usePostMessage
    fix: incorrect origin handling
    test: add tests

--- packages/core/src/bundle/hooks/usePostMessage/usePostMessage.js
+++ packages/core/src/bundle/hooks/usePostMessage/usePostMessage.js
@@ -17,12 +17,17 @@ import { useEffect, useRef } from 'react';
 export const usePostMessage = (origin, callback) => {
   const internalCallbackRef = useRef(callback);
   internalCallbackRef.current = callback;
+  const internalOriginRef = useRef(origin);
+  internalOriginRef.current = origin;
   useEffect(() => {
     const onMessage = (event) => {
-      if (
-        (Array.isArray(origin) && (!origin.includes(event.origin) || !origin.includes('*'))) ||
-        (event.origin !== origin && origin !== '*')
-      )
+      if (Array.isArray(internalOriginRef.current)) {
+        if (
+          !internalOriginRef.current.includes(event.origin) &&
+          !internalOriginRef.current.includes('*')
+        )
+          return;
+      } else if (internalOriginRef.current !== '*' && event.origin !== internalOriginRef.current)
         return;
       internalCallbackRef.current(event.data, event);
     };
@@ -30,11 +35,9 @@ export const usePostMessage = (origin, callback) => {
     return () => window.removeEventListener('message', onMessage);
   }, []);
   const postMessage = (message) => {
-    if (Array.isArray(origin)) {
-      origin.forEach((origin) => window.postMessage(message, origin));
-      return;
-    }
-    window.postMessage(message, origin);
+    if (Array.isArray(internalOriginRef.current))
+      return internalOriginRef.current.forEach((origin) => window.postMessage(message, origin));
+    window.postMessage(message, internalOriginRef.current);
   };
   return postMessage;
 };
