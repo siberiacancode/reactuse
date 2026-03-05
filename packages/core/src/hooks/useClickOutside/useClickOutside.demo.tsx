188aeda59c46a74ad47593ec52c83921a7cede58
debabin <dmitriybabinup@gmail.com>
2026-03-05 22:07:53 +0700

    main 🧊 delete destructure

--- packages/core/src/hooks/useClickOutside/useClickOutside.demo.tsx
+++ packages/core/src/hooks/useClickOutside/useClickOutside.demo.tsx
@@ -4,7 +4,7 @@ import { useClickOutside, useCounter } from '@siberiacancode/reactuse';
 const Demo = () => {
   const counter = useCounter();
 
-  const { ref: clickOutsideRef } = useClickOutside<HTMLDivElement>(() => {
+  const clickOutside = useClickOutside<HTMLDivElement>(() => {
     console.log('click outside');
     counter.inc();
   });
@@ -16,7 +16,7 @@ const Demo = () => {
       </p>
 
       <div
-        ref={clickOutsideRef}
+        ref={clickOutside.ref}
         className={cn(
           'relative flex flex-col items-center justify-center rounded-xl border-2 border-red-500 p-12',
           { 'border-green-500': counter.value > 5 }
