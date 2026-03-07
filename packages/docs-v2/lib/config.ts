0dd2baab13c39112de2a49f5a0a1896cc040c627
tungulin <tungulin2002@gmail.com>
2026-03-07 13:57:41 +0300

    feat: minor

--- packages/docs-v2/lib/config.ts
+++ packages/docs-v2/lib/config.ts
@@ -1,16 +1,15 @@
 export const siteConfig = {
-  name: "shadcn/ui",
-  url: "https://ui.shadcn.com",
-  ogImage: "https://ui.shadcn.com/og.jpg",
-  description:
-    "A set of beautifully designed components that you can customize, extend, and build on. Start here then make it your own. Open Source. Open Code.",
+  name: "reactuse",
+  url: "https://reactuse.org",
+  ogImage: "https://reactuse.org/logo.svg",
+  description: "-",
   links: {
     npm: "https://www.npmjs.com/package/@siberiacancode/reactuse",
     github: "https://github.com/siberiacancode/reactuse",
   },
   navItems: [
     {
-      href: "/docs/get-started",
+      href: "/docs/introduction",
       label: "Get started",
     },
     {
