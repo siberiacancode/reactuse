bf650d1dd524ac09ee59856ce513033e7ba67eb1
debabin <dmitriybabinup@gmail.com>
2026-05-03 16:57:35 +0700

    main 🧊 add hook to index

--- packages/newdocs/scripts/helpers/getElements.ts
+++ packages/newdocs/scripts/helpers/getElements.ts
@@ -1,7 +1,7 @@
 import fs from 'node:fs';
 import path from 'node:path';
 
-import type { FunctionType } from '../constants';
+import type { FunctionType } from '@/src/constants';
 
 import { CORE_ROOT } from '../constants';
 
@@ -11,6 +11,7 @@ export const getElements = async (type: FunctionType) => {
 
   return files
     .filter((file) => file.isDirectory())
+    .sort((left, right) => left.name.localeCompare(right.name))
     .map((file) => ({
       type,
       name: file.name
