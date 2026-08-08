38f1696067ff81f9d8861e37ec0bd371e05723ee
debabin <dmitriybabinup@gmail.com>
2026-08-08 13:13:01 +0700

    main 🧊 add blog code

--- packages/newdocs/app/(docs)/blog/page.tsx
+++ packages/newdocs/app/(docs)/blog/page.tsx
@@ -1,6 +1,7 @@
 import { blogSource } from '@docs/lib/source';
 import { RssIcon } from 'lucide-react';
 import Link from 'next/link';
+import process from 'node:process';
 
 import { Button } from '@/src/components/ui';
 
@@ -19,6 +20,20 @@ export const metadata = {
         }
       ]
     }
+  },
+  openGraph: {
+    title: 'Blog',
+    description: 'News, guides and updates about reactuse.',
+    type: 'website',
+    url: `${process.env.NEXT_PUBLIC_APP_URL}blog`,
+    images: [{ url: '/og.png', width: 1200, height: 630 }]
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Blog',
+    description: 'News, guides and updates about reactuse.',
+    images: ['/og.png'],
+    creator: '@siberiacancode'
   }
 };
 
