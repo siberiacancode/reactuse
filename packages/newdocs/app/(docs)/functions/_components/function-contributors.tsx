48ffecab8f26c382fa1c1457d80c20dad7db7219
debabin <dmitriybabinup@gmail.com>
2026-08-11 00:42:20 +0700

    main 🧊 update badge

--- packages/newdocs/app/(docs)/functions/_components/function-contributors.tsx
+++ packages/newdocs/app/(docs)/functions/_components/function-contributors.tsx
@@ -10,12 +10,12 @@ interface FunctionContributorsProps {
 export const FunctionContributors = (props: FunctionContributorsProps) => (
   <div className='my-4 flex flex-wrap gap-4'>
     {props.contributors.map(({ name, avatar }) => (
-      <Badge key={name} className='flex items-center gap-2' variant='outline'>
-        <Avatar>
+      <Badge key={name} className='h-8 gap-2 rounded-full px-2 py-0 pl-1' variant='outline'>
+        <Avatar size='sm'>
           <AvatarImage alt={name} src={avatar} />
           <AvatarFallback>{name.charAt(0)}</AvatarFallback>
         </Avatar>
-        <span className='text-lg'>{name}</span>
+        <span className='text-sm'>{name}</span>
       </Badge>
     ))}
   </div>
